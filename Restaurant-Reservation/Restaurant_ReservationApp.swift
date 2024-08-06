6e85134e95f04f2113b8c7bbd2cae652f810cd61
yuji090 <bishtakhil04@gmail.com>
2024-08-07 02:29:31 +0530

    Initial Commit

---
+++ Restaurant-Reservation/Restaurant_ReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_ReservationApp.swift
+//  Restaurant-Reservation
+//
+//  Created by Akhil Bisht on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_ReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restaurant-ReservationUITests/Restaurant_ReservationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Restaurant_ReservationUITestsLaunchTests.swift
+//  Restaurant-ReservationUITests
+//
+//  Created by Akhil Bisht on 07/08/24.
+//
+
+import XCTest
+
+final class Restaurant_ReservationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
